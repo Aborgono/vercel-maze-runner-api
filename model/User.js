f8492c4801d596b3a6205cbf649f39889428eb0e
Aborgono <albertoiborgonovo@gmail.com>
2023-09-18 20:04:39 -0400

    added server port and model for schema

---
+++ model/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+    },
+    difficulty: {
+        type: String,
+        require: true,
+    },
+    score: {
+        type: Number,
+    }
+})
