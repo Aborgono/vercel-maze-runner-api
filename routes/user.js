989a9de0ac4bb80d442b9dfe63ceeb190a701284
Aborgono <albertoiborgonovo@gmail.com>
2023-09-18 23:33:40 -0400

    brainstorming ideas

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = require('express').Router()
+
+const database = 'Maze-Runner'
+const collection = 'users'
+
+export const userMethod = (req, res, db) => {
+    
+}
+use(database);
+
+// router.post('/',async(req, res) => {
+//     const newUser = req.body;
+//     try {
+//         const addUser = await
+//     } catch (error) {
+        
+//     }
+// })
+
+module.exports = userMethod
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require ('express')
 const cors = require ('cors')
-const mongoose = require ('mongoose')
+const { userMethod } = require('./routes/user')
+// const mongoose = require ('mongoose')
 const app = express()
 const PORT = 8080
 
+
 app.use(cors())
 app.use(express.json())
+
+app.use('/users', userMethod)
 
 app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`);
