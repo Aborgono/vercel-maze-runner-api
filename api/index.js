cce41084def6a3ab7935d3182d798bfc8342ea26
Aborgono <albertoiborgonovo@gmail.com>
2023-10-30 15:47:44 -0400

    cleaned up index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,3 @@
-// const express = require('express');
-// const { v4 } = require('uuid');
-
-// // Create an instance of Express.js
-// const app = express();
-
-// // Set Cache-Control headers for all responses from this Express app
-// app.use((req, res, next) => {
-//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-//   next();
-// });
-
-// // Define the API routes
-// app.get('/api', (req, res) => {
-//   const path = `/api/item/${v4()}`;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-// });
-
-
-// module.exports = app;
 
 const express = require ('express')
 const app = express()
