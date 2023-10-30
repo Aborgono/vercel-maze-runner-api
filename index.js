97776b1a4776fd75d0a3ce477c39bc92bba81a64
Aborgono <albertoiborgonovo@gmail.com>
2023-10-30 19:37:12 -0400

    changed configuration of index.js

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-// {
-//     "rewrites": [{ "source": "/api/(.*)", "destination": "/api" }]
-//   } 
-
-
-// {
-//   "version": 2,
-//   "builds": [
-//     {
-//       "src": "index.js",
-//       "use": "@now/node"
-//     }
-//   ],
-//   "routes": [
-//     {
-//       "src": "/(.*)",
-//       "dest": "index.js"
-//     }
-//   ]
-// }
-
 const express = require ('express')
 const app = express()
 // const cors = require ('cors')
 const PORT = 8080
 // const mongoose = require ('mongoose')
+
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`);
+})
 
 const userRoute = require('./routes/user')
 const leaderBoardRoute = require('./routes/leaderBoard')
@@ -34,10 +17,6 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use('/users', userRoute)
 app.use('/leaderBoard', leaderBoardRoute)
-
-app.listen(PORT, () => {
-    console.log(`App is running on ${PORT}`);
-})
 
 app.get('/', (req, res) => {
     res.send('Hey this is my API running 🥳')
