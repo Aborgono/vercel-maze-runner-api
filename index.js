ada63a7ae1badbc7f2b8e757861855ad1dd74c3c
Aborgono <albertoiborgonovo@gmail.com>
2023-10-31 13:19:58 -0400

    changed folder structure of routes

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`);
 })
 
-const userRoute = require('./routes/user')
-const leaderBoardRoute = require('./routes/leaderBoard')
+const userRoute = require('./pages/api/user')
+const leaderBoardRoute = require('./pages/api/leaderBoard')
 const bodyParser = require('body-parser')
 
 
