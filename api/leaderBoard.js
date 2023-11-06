1d9373a82e908be7c04b985898c01b2303f01659
Aborgono <albertoiborgonovo@gmail.com>
2023-11-05 20:54:09 -0500

    console.log everything

--- api/leaderBoard.js
+++ api/leaderBoard.js
@@ -3,22 +3,31 @@ const router = express.Router();
 const { connectToDatabase, closeDatabaseConnection, client } = require('../mongoDBConfig/mongodb');
 
 router.get("/", async (req, res) => {
+    console.log("1")
     try {
         // Connect to the database
+        console.log("2")
         await connectToDatabase(client);
+        console.log("3")
         const db = client.db("Maze-Runner");
+        console.log("4")
         const coll = db.collection("users");
+        console.log("5")
 
         // Get user data by finding all users
         const cursor = await coll.find();
+        console.log("6")
         const userData = await cursor.toArray();
+        console.log("7")
 
         res.json(userData);
+        console.log("8")
     } catch (error) {
         console.error("Error retrieving user data:", error);
         res.status(500).json({ error: "Internal server error" });
     } finally {
         // Close the database connection
+        console.log("9")
         await closeDatabaseConnection();
     }
 });
