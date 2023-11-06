3a7aa8a8f38c5a5dde615d6a3691992e674c1e9e
Aborgono <albertoiborgonovo@gmail.com>
2023-11-05 19:13:26 -0500

    changed routes once more

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express()
 const PORT = 8080
 // const mongoose = require ('mongoose')
 
-const { connectToDatabase, closeDatabaseConnection, client, insertUser } = require('../mongoDBConfig/mongodb');
+// const { connectToDatabase, closeDatabaseConnection, client, insertUser } = require('../mongoDBConfig/mongodb');
 
 
 
@@ -14,67 +14,67 @@ app.listen(PORT, () => {
     console.log(`App is running on ${PORT}`);
 })
 
-// const userRoute = require('../pages/user')
-// const leaderBoardRoute = require('../pages/leaderBoard')
+const userRoute = require('../pages/user')
+const leaderBoardRoute = require('../pages/leaderBoard')
 const bodyParser = require('body-parser')
 
 
-// app.use(cors())
+app.use(cors())
 app.use(bodyParser.json())
-// app.use('/users', userRoute)
-// app.use('/leaderBoard', leaderBoardRoute)
+app.use('/users', userRoute)
+app.use('/leaderBoard', leaderBoardRoute)
 
 app.get('/', (req, res) => {
     res.send('Hey this is my API running 🥳')
   })
 
-// app.get('/users', (req, res) => {
+// router.get('/users', (req, res) => {
 //     res.send('Users route');
 //   });
 
 
 // router.post("/", async (req, res) => {
-app.post("/api/users", async (req, res) => {
-  try {
-      await connectToDatabase()
-      const anonymousUser = req.body;
-      console.log("Received user data", anonymousUser);
+// app.post("/api/users", async (req, res) => {
+//   try {
+//       await connectToDatabase()
+//       const anonymousUser = req.body;
+//       console.log("Received user data", anonymousUser);
 
-      // Insert the user into the database using the function from db.js
-      const insertedId = await insertUser(anonymousUser);
+//       // Insert the user into the database using the function from db.js
+//       const insertedId = await insertUser(anonymousUser);
 
-      res.json({ message: "User inserted successfully", insertedId });
-  } catch (error) {
-      console.error("Error inserting user:", error);
-      res.status(500).json({ error: "Internal server error" });
-  } finally {
-      await closeDatabaseConnection();
-  }
-});
+//       res.json({ message: "User inserted successfully", insertedId });
+//   } catch (error) {
+//       console.error("Error inserting user:", error);
+//       res.status(500).json({ error: "Internal server error" });
+//   } finally {
+//       await closeDatabaseConnection();
+//   }
+// });
 
   // router.get("/leaderBoard", async (req, res) => {
-app.get("/api/leaderboard", async (req, res) => {
-  try {
-      // Connect to the database
-      await connectToDatabase(client);
-      const db = client.db("Maze-Runner");
-      const coll = db.collection("users");
+// app.get("/api/leaderboard", async (req, res) => {
+//   try {
+//       // Connect to the database
+//       await connectToDatabase(client);
+//       const db = client.db("Maze-Runner");
+//       const coll = db.collection("users");
 
-      // Get user data by finding all users
-      const cursor = await coll.find();
-      const userData = await cursor.toArray();
+//       // Get user data by finding all users
+//       const cursor = await coll.find();
+//       const userData = await cursor.toArray();
 
-      res.json(userData);
-  } catch (error) {
-      console.error("Error retrieving user data:", error);
-      res.status(500).json({ error: "Internal server error" });
-  } finally {
-      // Close the database connection
-      await closeDatabaseConnection();
-  }
-});  
+//       res.json(userData);
+//   } catch (error) {
+//       console.error("Error retrieving user data:", error);
+//       res.status(500).json({ error: "Internal server error" });
+//   } finally {
+//       // Close the database connection
+//       await closeDatabaseConnection();
+//   }
+// });  
 
-// app.get('/leaderBoard', (req, res) => {
+// router.get('/', (req, res) => {
 //     res.send('Leaderboard route');
 //   });
 
